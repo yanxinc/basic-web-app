dbc8c22adf35e783005cc81f52804c3994dd6ca2
cindycyx <yanxinc@andrew.cmu.edu>
2023-03-14 16:03:52 -0400

    max number

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -9,6 +9,23 @@ export default function QueryProcessor(query: string): string {
   if (query.includes("What is your name?")) {
     return "abcd";
   }
+  // if (query.includes("What is 21 plus 46?")) {
+  //   return "67";
+  // }
+
+  if (query.includes("Which of the following numbers is the largest")) {
+    var number = query.split(": ")[1].replace('?', "");
+    var numbers = number.split(", ")
+
+    var max = 0;
+    for (let i = 0; i < 3; i++){
+      if (parseInt(numbers[i]) > max){
+        max = parseInt(numbers[i])
+      }
+    }
+
+    return max.toString();
+  }
 
   return "";
 }
